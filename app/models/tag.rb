d7c5b97fe6ac03aa0c0ba9e1603e207c25fde9e0
Cirdes Henrique <cirdes@gmail.com>
2012-06-21 15:26:25 -0300

    translate shortcode_sensor

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,19 @@
 class Tag < ActiveRecord::Base
-  attr_accessible :area, :epc, :shortcode_sensor, :timestamp
+  attr_accessible :epc, :shortcode_sensor, :timestamp
+
+  before_validation :set_area
+
+  AREAS = %w[quarto sala cozinha]
+
+
+  private
+  def set_area
+    tags_file = Rails.root.join("config/tags.yml")
+    tags = YAML.load_file(tags_file)
+    Tag::AREAS.each do |area|
+      if tags[area].has_value?(self.shortcode_sensor.to_i)
+        self.area = area
+      end
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120621144021) do
+ActiveRecord::Schema.define(:version => 20120621145041) do
 
   create_table "tags", :force => true do |t|
     t.string   "shortcode_sensor"
